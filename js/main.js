3a0535aa4c34a1d7ee8faf13735fafc044e564da
AndreyKondakov <primax7k@gmail.com>
2019-03-27 10:30:18 +0200

    one more

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // register ServiceWorker, remember to use absolute path!
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/andreykondakov.github.io/sw.js', {scope: '/andreykondakov.github.io/'})
-}
+// if (navigator.serviceWorker) {
+//   navigator.serviceWorker.register('/andreykondakov.github.io/sw.js', {scope: '/andreykondakov.github.io/'})
+// }
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
 
 var geekhub = document.getElementById('btn-last-change');
 geekhub.addEventListener('click', function () {
